01acdcb4251aeac9e4911b6055ea99c42445e540
Roberto Mannai <robermann@gmail.com>
2018-09-05 20:24:45 +0200

    third transaction, fixed change address and check errors

--- NBitcoinFirstProject/FirstTestnetTransaction.cs
+++ NBitcoinFirstProject/FirstTestnetTransaction.cs
@@ -17,6 +17,7 @@ namespace NBitcoinFirstProject
         {
             //Transaction1();
             //Transaction2();
+            Transaction3();
         }
 
         //transaction https://live.blockcypher.com/btc-testnet/tx/4761929185a3dd80f23670efb854ba3d7ab5a151f296ea7c38015ceb821ee838/
@@ -53,8 +54,9 @@ namespace NBitcoinFirstProject
             builder.AddKeys(bitcoinPrivateKey);
             builder.Send(destinationAddress, new Money(0.000000015m, MoneyUnit.BTC));
             builder.SendFees(Money.Coins(0.001m));
-            builder.SetChange(address); //<--- **** the difference didn't come back to the change address!
+            builder.SetChange(address.ScriptPubKey);
             var signedTx = builder.BuildTransaction(true);
+            String hexPreVerify = signedTx.ToHex();
             Boolean b = builder.Verify(signedTx);
             String hex = signedTx.ToHex(); //"01000000000101e38fd8de708abdb8eddd0e47d1369f44d4e2de0d5407cf277d4dfc73a0a922875300000017160014a017d15c9cfacea59c4a3984b32f2282e617de8effffffff01f6b4b600000000001976a914a017d15c9cfacea59c4a3984b32f2282e617de8e88ac02473044022028f8440e4d85eca57a99fadda133fc9ce48b523a4b4d2438aaaad0a296d8d7270220112eb54f7e8a1f311b6b4dc0a15c4b3a5311948724a8aa0657da52da6b26fc68012102b6d3f00966c6797c69b98cc7d4f7ec0e9ec8aee68f2fccb5cec061eeaee47f8900000000"
 
@@ -190,12 +192,63 @@ namespace NBitcoinFirstProject
             }
         }
 
-        public static byte[] ToByteArray(String hexString)
+        //transaction https://live.blockcypher.com/btc-testnet/tx/ca678d531e9c8a3cd95e1e33a2562cf0b8a44793e51b46ac7602a72cbe7e1bd0/
+        public static void Transaction3()
         {
-            byte[] retval = new byte[hexString.Length / 2];
-            for (int i = 0; i < hexString.Length; i += 2)
-                retval[i / 2] = Convert.ToByte(hexString.Substring(i, 2), 16);
-            return retval;
+
+            var bitcoinPrivateKey = new BitcoinSecret("cS88F3sJycinEUfGGMQH4w4izaNjEec97VAHsoQZYmDjZJwK1wuf");
+            var key = bitcoinPrivateKey.PrivateKey;
+
+            var network = bitcoinPrivateKey.Network;
+            var address = bitcoinPrivateKey.GetAddress();
+
+            var client = new QBitNinjaClient(network);
+            var transactionId = uint256.Parse("63ea3b0cac9e9afc188b42785a63f5bc84dd120400a5e18eccbbbb82adeadf2c");
+
+
+            var transactionResponse = client.GetTransaction(transactionId).Result;
+            Coin[] coinsToSpend = transactionResponse.Transaction.Outputs.AsCoins().ToArray();
+            var destinationAddress = BitcoinAddress.Create("2NFdSeH15r3ua6x1pV8zVg18pwBz8Rxy285", network);
+
+
+            var coins =
+                coinsToSpend
+                .Where(c => c.TxOut.ScriptPubKey == bitcoinPrivateKey.ScriptPubKey)
+                .ToArray();
+            
+            TransactionBuilder builder = new TransactionBuilder();
+            builder.AddCoins(coins);
+            builder.AddKeys(bitcoinPrivateKey);
+            builder.Send(destinationAddress, new Money(0.02m, MoneyUnit.BTC));
+            builder.SendFees(Money.Coins(0.001m));
+            builder.SetChange(address.ScriptPubKey);
+            var signedTx = builder.BuildTransaction(true);
+            String hexPreVerify = signedTx.ToHex();
+
+            NBitcoin.Policy.TransactionPolicyError[] errors =  null;
+            Boolean b = builder.Verify(signedTx, out errors);
+            if (errors != null)
+            {
+                foreach (var e in errors)
+                {
+                    Console.WriteLine("builder error-> " + e.ToString());
+                }
+            }
+            String hex = signedTx.ToHex(); //01000000012cdfeaad82bbbbcc8ee1a5000412dd84bcf5635a78428b18fc9a9eac0c3bea63010000006b48304502210088bd03eec8b7b4ac84e76724354b139da50a8f7028a29a0b2eef8071965a9d7c02205ddbd388bab940dfe18bb4863983473b0dbdc475048b8216560931cf2e46e1ae012102b6d3f00966c6797c69b98cc7d4f7ec0e9ec8aee68f2fccb5cec061eeaee47f89ffffffff020c069600000000001976a914a017d15c9cfacea59c4a3984b32f2282e617de8e88ac80841e000000000017a914f5867b7847aad72d7f74675391a5945c5c43268a8700000000
+
+            BroadcastResponse broadcastResponse = client.Broadcast(signedTx).Result;
+
+            if (!broadcastResponse.Success)
+            {
+                Console.Error.WriteLine("ErrorCode: " + broadcastResponse.Error.ErrorCode);
+                Console.Error.WriteLine("Error message: " + broadcastResponse.Error.Reason);
+            }
+            else
+            {
+                Console.WriteLine("Success! You can check out the hash of the transaciton in any block explorer:");
+                Console.WriteLine(signedTx.GetHash()); //id: ca678d531e9c8a3cd95e1e33a2562cf0b8a44793e51b46ac7602a72cbe7e1bd0
+            }
         }
+        
     }
 }
